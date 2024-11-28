4e9621957773778c78382b346179c919229516c8
Malefic <om@malefic.xyz>
2024-11-28 14:56:06 -0800

    fix(tree): made tree functions generic type
    should probably move them out of file package as a result

--- src/main/kotlin/xyz/malefic/extensions/standard/file/Tree.kt
+++ src/main/kotlin/xyz/malefic/extensions/standard/file/Tree.kt
@@ -9,7 +9,48 @@ import java.io.File
  * @param value The value of the node.
  * @param children The children of the node.
  */
-data class TreeNode<T>(val value: T, val children: List<TreeNode<T>> = emptyList())
+data class TreeNode<T>(val value: T, val children: List<TreeNode<T>> = emptyList()) {
+  /**
+   * Returns the string representation of the tree node.
+   *
+   * @return The string representation of the tree node.
+   */
+  override fun toString(): String = buildString {
+    append(value.toString())
+    if (children.isNotEmpty()) {
+      append(" { ")
+      children.joinTo(this, separator = ", ") { it.toString() }
+      append(" }")
+    }
+  }
+
+  /**
+   * Flattens a tree structure into a list of pairs.
+   *
+   * @param depth The current depth of the node.
+   * @return A list of pairs containing the file and its depth.
+   */
+  fun flattenTree(depth: Int = 0): List<Pair<T, Int>> =
+    mutableListOf<Pair<T, Int>>().also {
+      it.add(value to depth)
+      children.forEach { child -> it.addAll(child.flattenTree(depth + 1)) }
+    }
+
+  /**
+   * Filters the tree nodes based on a given predicate.
+   *
+   * This function recursively filters the tree nodes. If a node does not satisfy the predicate, it
+   * will be excluded from the resulting tree.
+   *
+   * @param predicate The condition to filter the nodes.
+   * @return The filtered tree node or null if the node does not satisfy the predicate.
+   */
+  fun filter(predicate: (T) -> Boolean): TreeNode<T>? {
+    if (!predicate(this.value)) return null
+    val filteredChildren = this.children.mapNotNull { it.filter(predicate) }
+    return TreeNode(this.value, filteredChildren)
+  }
+}
 
 /**
  * Recursively builds a file tree from a given file.
@@ -24,31 +65,3 @@ data class TreeNode<T>(val value: T, val children: List<TreeNode<T>> = emptyList
  */
 fun File.buildFileTree(): TreeNode<File> =
   TreeNode(this, listFiles()?.map { it.buildFileTree() } ?: emptyList())
-
-/**
- * Flattens a tree structure into a list of pairs.
- *
- * @param node The root node of the tree.
- * @param depth The current depth of the node.
- * @return A list of pairs containing the file and its depth.
- */
-fun flattenTree(node: TreeNode<File>, depth: Int = 0): List<Pair<File, Int>> =
-  mutableListOf<Pair<File, Int>>().also {
-    it.add(node.value to depth)
-    node.children.forEach { child -> it.addAll(flattenTree(child, depth + 1)) }
-  }
-
-/**
- * Filters the tree nodes based on a given predicate.
- *
- * This function recursively filters the tree nodes. If a node does not satisfy the predicate, it
- * will be excluded from the resulting tree.
- *
- * @param predicate The condition to filter the nodes.
- * @return The filtered tree node or null if the node does not satisfy the predicate.
- */
-fun TreeNode<File>.filter(predicate: (File) -> Boolean): TreeNode<File>? {
-  if (!predicate(this.value)) return null
-  val filteredChildren = this.children.mapNotNull { it.filter(predicate) }
-  return TreeNode(this.value, filteredChildren)
-}
