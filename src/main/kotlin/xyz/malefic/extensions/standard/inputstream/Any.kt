c4d6db3a12b17f56f73690c5ef5d04c44f6a4119
Malefic <om@malefic.xyz>
2024-12-08 16:07:17 -0800

    feat(standard): add function to get inputstream resource
    grass is a much better name for that

---
+++ src/main/kotlin/xyz/malefic/extensions/standard/inputstream/Any.kt
@@ -0,0 +1,11 @@
+package xyz.malefic.extensions.standard.inputstream
+
+import java.io.InputStream
+
+/**
+ * Function to get an InputStream for a given resource string.
+ *
+ * @param string the resource path as a string
+ * @return InputStream for the resource, or null if the resource is not found
+ */
+fun Any.grass(string: String): InputStream? = this::class.java.getResourceAsStream(string)
