0f2ffa8d06d7bfcaa26fcebc1408206f55f4cfb2
Malefic <om@malefic.xyz>
2024-12-11 13:40:43 -0800

    feat(composables): generic composable extension functions for spacers
    version 1.1.5! yayyyy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import cn.lalaki.pub.BaseCentralPortalPlusExtension.PublishingType
 
-val v = "1.1.4"
+val v = "1.1.5"
 val localMavenRepo = uri(layout.buildDirectory.dir("repo").get())
 
 plugins {
